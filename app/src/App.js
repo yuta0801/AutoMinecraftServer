a5859569f6695cb75bc1c6cabcd717d08ceaadf0
yuta0801 <yuta20030801@icloud.com>
2019-04-28 08:00:05 +0900

    Remove semicolons

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">content</div>
-  );
+  )
 }
 
-export default App;
+export default App
