09f8108766003c8a779e3e679265f60e090b2e87
yuta0801 <yuta20030801@icloud.com>
2019-09-07 03:51:45 +0900

    Toggle overlay menu when window size changed

--- app/src/components/Menu.tsx
+++ app/src/components/Menu.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import New from './New'
 import News from './News'
-const { useState } = React
 
 const Menu = () => {
   const [isOpen, setMenu] = useState(false)
+
+  useEffect(() => {
+    const handler = () => {
+      const width = document.documentElement.clientWidth
+      if (width > 1380) setMenu(false)
+    }
+    window.addEventListener('resize', handler)
+    return () => window.removeEventListener('resize', handler)
+  }, [])
 
   return (
     <div>
@@ -12,14 +20,16 @@ const Menu = () => {
         <a id="menu_title">MENU</a>
         <div id="menu_icon"></div>
       </div>
-      { isOpen && <div id="menu" data-show="false">
-        <div className="col-xs-6 s-pad">
-          <News />
+      { isOpen && (
+        <div id="menu" data-show="false">
+          <div className="col-xs-6 s-pad">
+            <News />
+          </div>
+          <div className="col-xs-6 s-pad">
+            <New />
+          </div>
         </div>
-        <div className="col-xs-6 s-pad">
-          <New />
-        </div>
-      </div> }
+      ) }
     </div>
   )
 }
