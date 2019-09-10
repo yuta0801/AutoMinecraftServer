0a49bf6666c85c7f241338b741b3acde0945ccb1
yuta0801 <yuta20030801@icloud.com>
2019-09-10 18:42:31 +0900

    Move constants to one file

--- app/src/components/ProfileModal.tsx
+++ app/src/components/ProfileModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import useModal from '../hooks/useModal'
 import { Profile } from '../types'
+import { PROFILE_DEFAULT, VERSIONS } from '../constants'
+import useModal from '../hooks/useModal'
 import { Slider, Dropdown } from './atoms/Form'
 
 interface ProfileModalProps {
@@ -9,47 +10,6 @@ interface ProfileModalProps {
   profile?: Profile
   new?: boolean
 }
-
-const PROFILE_DEFAULT = {
-  name: '',
-  folder: '',
-  jar: '',
-  max_memory: 1024,
-  min_memory: 512,
-  upnp: true,
-  backup: true,
-  backup_minute: '10',
-  backup_count: '5',
-}
-
-const VERSION = [
-  'バニラサーバー',
-  ...[
-    'Vanila 1.12.2', 'Vanila 1.12.1', 'Vanila 1.12', 'Vanila 1.11.2',
-    'Vanila 1.11.1', 'Vanila 1.11', 'Vanila 1.10.2', 'Vanila 1.10.1',
-    'Vanila 1.10', 'Vanila 1.9.4', 'Vanila 1.9.3', 'Vanila 1.9.2',
-    'Vanila 1.9.1', 'Vanila 1.9', 'Vanila 1.8.9', 'Vanila 1.8.8',
-    'Vanila 1.8.7', 'Vanila 1.8.6', 'Vanila 1.8.5', 'Vanila 1.8.4',
-    'Vanila 1.8.3', 'Vanila 1.8.2', 'Vanila 1.8.1', 'Vanila 1.8',
-    'Vanila 1.7.10', 'Vanila 1.7.9', 'Vanila 1.7.5', 'Vanila 1.7.4',
-    'Vanila 1.7.2', 'Vanila 1.6.4', 'Vanila 1.6.2', 'Vanila 1.6.1',
-    'Vanila 1.5.2', 'Vanila 1.5.1', 'Vanila 1.4.7', 'Vanila 1.4.6',
-    'Vanila 1.4.5', 'Vanila 1.4.4', 'Vanila 1.4.2', 'Vanila 1.3.2',
-    'Vanila 1.3.1', 'Vanila 1.2.5', 'Vanila 1.2.4', 'Vanila 1.2.3',
-    'Vanila 1.2.2', 'Vanila 1.2.1', 'Vanila 1.1', 'Vanila 1.0',
-  ].map(version => ({ value: version })),
-  'Forgeサーバー',
-  ...[
-    'Forge 1.12.2', 'Forge 1.12.1', 'Forge 1.12', 'Forge 1.11.2', 'Forge 1.11',
-    'Forge 1.10.2', 'Forge 1.10', 'Forge 1.9.4', 'Forge 1.9', 'Forge 1.8.9',
-    'Forge 1.8.8', 'Forge 1.8', 'Forge 1.7.10', 'Forge 1.7.2', 'Forge 1.6.4',
-    'Forge 1.6.3', 'Forge 1.6.2', 'Forge 1.6.1', 'Forge 1.5.2',
-    // 'Forge 1.5.1', 'Forge 1.5', 'Forge 1.4.7', 'Forge 1.4.6',
-    // 'Forge 1.4.5', 'Forge 1.4.4', 'Forge 1.4.3', 'Forge 1.4.2',
-    // 'Forge 1.4.1', 'Forge 1.4.0', 'Forge 1.3.2', 'Forge 1.2.5',
-    // 'Forge 1.2.4', 'Forge 1.2.3', 'Forge 1.1',
-  ].map(version => ({ value: version })),
-]
 
 const ProfileModal = (props: ProfileModalProps) => {
   const [state, setState] = useState(props.profile || PROFILE_DEFAULT)
@@ -80,7 +40,7 @@ const ProfileModal = (props: ProfileModalProps) => {
           { !props.new && <dd><label><input id="change_check" type="checkbox" onChange={e => toggleVersionChange(e.target.checked)} /> Minecraftバージョンを変更</label></dd> }
           { versionChange && (
             <dd>
-              <Dropdown options={VERSION} value={version} onChange={version => toggleVersion(version)}></Dropdown>
+              <Dropdown options={VERSIONS} value={version} onChange={version => toggleVersion(version)}></Dropdown>
               <label><input id="latest_check" type="checkbox" />latest(最新版、Forgeのみ)</label>
             </dd>
           ) }
---
+++ app/src/constants.ts
@@ -0,0 +1,40 @@
+export const PROFILE_DEFAULT = {
+  name: '',
+  folder: '',
+  jar: '',
+  max_memory: 1024,
+  min_memory: 512,
+  upnp: true,
+  backup: true,
+  backup_minute: '10',
+  backup_count: '5',
+}
+
+export const VERSIONS = [
+  'バニラサーバー',
+  ...[
+    'Vanila 1.12.2', 'Vanila 1.12.1', 'Vanila 1.12', 'Vanila 1.11.2',
+    'Vanila 1.11.1', 'Vanila 1.11', 'Vanila 1.10.2', 'Vanila 1.10.1',
+    'Vanila 1.10', 'Vanila 1.9.4', 'Vanila 1.9.3', 'Vanila 1.9.2',
+    'Vanila 1.9.1', 'Vanila 1.9', 'Vanila 1.8.9', 'Vanila 1.8.8',
+    'Vanila 1.8.7', 'Vanila 1.8.6', 'Vanila 1.8.5', 'Vanila 1.8.4',
+    'Vanila 1.8.3', 'Vanila 1.8.2', 'Vanila 1.8.1', 'Vanila 1.8',
+    'Vanila 1.7.10', 'Vanila 1.7.9', 'Vanila 1.7.5', 'Vanila 1.7.4',
+    'Vanila 1.7.2', 'Vanila 1.6.4', 'Vanila 1.6.2', 'Vanila 1.6.1',
+    'Vanila 1.5.2', 'Vanila 1.5.1', 'Vanila 1.4.7', 'Vanila 1.4.6',
+    'Vanila 1.4.5', 'Vanila 1.4.4', 'Vanila 1.4.2', 'Vanila 1.3.2',
+    'Vanila 1.3.1', 'Vanila 1.2.5', 'Vanila 1.2.4', 'Vanila 1.2.3',
+    'Vanila 1.2.2', 'Vanila 1.2.1', 'Vanila 1.1', 'Vanila 1.0',
+  ].map(version => ({ value: version })),
+  'Forgeサーバー',
+  ...[
+    'Forge 1.12.2', 'Forge 1.12.1', 'Forge 1.12', 'Forge 1.11.2', 'Forge 1.11',
+    'Forge 1.10.2', 'Forge 1.10', 'Forge 1.9.4', 'Forge 1.9', 'Forge 1.8.9',
+    'Forge 1.8.8', 'Forge 1.8', 'Forge 1.7.10', 'Forge 1.7.2', 'Forge 1.6.4',
+    'Forge 1.6.3', 'Forge 1.6.2', 'Forge 1.6.1', 'Forge 1.5.2',
+    // 'Forge 1.5.1', 'Forge 1.5', 'Forge 1.4.7', 'Forge 1.4.6',
+    // 'Forge 1.4.5', 'Forge 1.4.4', 'Forge 1.4.3', 'Forge 1.4.2',
+    // 'Forge 1.4.1', 'Forge 1.4.0', 'Forge 1.3.2', 'Forge 1.2.5',
+    // 'Forge 1.2.4', 'Forge 1.2.3', 'Forge 1.1',
+  ].map(version => ({ value: version })),
+]
