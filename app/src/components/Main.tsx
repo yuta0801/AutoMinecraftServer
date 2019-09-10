37cb1265d30f5b7843f17c2a7fa771bab150f215
yuta0801 <yuta20030801@icloud.com>
2019-09-10 15:16:29 +0900

    Move dummy data to outside file

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -2,34 +2,7 @@ import React from 'react'
 import New from './New'
 import News from './News'
 import Servers from './Servers'
-
-const dummyLogs = [
-  ['00:00:00', 'Info', 'Starting minecraft server version 1.12'],
-  ['00:00:00', 'Info', 'Loading properties'],
-  ['00:00:00', 'Info', 'server.properties does not exist'],
-  ['00:00:00', 'Info', 'Generating new properties file'],
-  ['00:00:00', 'Info', 'Failed to load eula.txt'],
-  ['00:00:00', 'Info', 'You need to agree to the EULA in order to run the server. Go to eula.txt for more info.'],
-  ['00:00:00', 'Info', 'Stopping server'],
-  ['00:00:00', 'Info', 'Stopping server'],
-]
-
-const dummyProfile = {
-  name: 'Hoge',
-  folder: '/path/to/server',
-  jar: '/path/to/server/minecraft_server.jar',
-  max_memory: 1024,
-  min_memory: 512,
-  upnp: true,
-  backup: true,
-  backup_minute: '10',
-  backup_count: '5'
-}
-
-const dummyData = {
-  a: { id: 'a', name: 'hoge', status: 'stopped', log: dummyLogs, profile: dummyProfile },
-  b: { id: 'b', name: 'fuga', status: 'running', log: dummyLogs, profile: dummyProfile },
-} as const
+import { dummyServers } from '../dummy'
 
 const Main = () => {
   return (
@@ -45,7 +18,7 @@ const Main = () => {
         <New />
       </div>
       <div id="main_right" className="col-xs-8 s-pad">
-        <Servers servers={dummyData} />
+        <Servers servers={dummyServers} />
       </div>
     </div>
   )
---
+++ app/src/dummy.ts
@@ -0,0 +1,29 @@
+import { Profile, Server } from './types'
+
+const dummyLogs = [
+  ['00:00:00', 'Info', 'Starting minecraft server version 1.12'],
+  ['00:00:00', 'Info', 'Loading properties'],
+  ['00:00:00', 'Info', 'server.properties does not exist'],
+  ['00:00:00', 'Info', 'Generating new properties file'],
+  ['00:00:00', 'Info', 'Failed to load eula.txt'],
+  ['00:00:00', 'Info', 'You need to agree to the EULA in order to run the server. Go to eula.txt for more info.'],
+  ['00:00:00', 'Info', 'Stopping server'],
+  ['00:00:00', 'Info', 'Stopping server'],
+]
+
+const dummyProfile: Profile = {
+  name: 'Hoge',
+  folder: '/path/to/server',
+  jar: '/path/to/server/minecraft_server.jar',
+  max_memory: 1024,
+  min_memory: 512,
+  upnp: true,
+  backup: true,
+  backup_minute: '10',
+  backup_count: '5'
+}
+
+export const dummyServers: Record<string, Server> = {
+  a: { id: 'a', name: 'hoge', status: 'stopped', log: dummyLogs, profile: dummyProfile },
+  b: { id: 'b', name: 'fuga', status: 'running', log: dummyLogs, profile: dummyProfile },
+}
