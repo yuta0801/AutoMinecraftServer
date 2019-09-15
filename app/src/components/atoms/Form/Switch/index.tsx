d0546145703f2ab3f2b914ad264f1e169ecd4d78
yuta0801 <yuta20030801@icloud.com>
2019-09-15 23:44:26 +0900

    Add toggle logic to Switch component

--- app/src/components/atoms/Form/Switch/index.tsx
+++ app/src/components/atoms/Form/Switch/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Wrapper, Container, Handle, Label } from './style'
 
 interface SwitchProps {
@@ -9,13 +9,14 @@ interface SwitchProps {
 }
 
 const Switch = (props: SwitchProps) => {
+  const [value, setState] = useState(true)
   return (
-    <Wrapper className="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-mini bootstrap-switch-id-snooper-enabled bootstrap-switch-animate bootstrap-switch-on">
-      <Container className="bootstrap-switch-container">
+    <Wrapper className="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-mini bootstrap-switch-id-snooper-enabled bootstrap-switch-animate" tabIndex={0} onClick={() => setState(!value)}>
+      <Container switch={value} className="bootstrap-switch-container">
         <Handle className="bootstrap-switch-handle-on bootstrap-switch-primary">有効</Handle>
         <Label className="bootstrap-switch-label">&nbsp;</Label>
         <Handle className="bootstrap-switch-handle-off bootstrap-switch-default">無効</Handle>
-        <input id={props.id} className="properties_toggle" type="checkbox" name="toggle" checked={props.value} defaultChecked={props.defaultChecked} onChange={e => props.onChange && props.onChange(e.target.checked)} />
+        <input id={props.id} className="properties_toggle" type="checkbox" name="toggle" checked={props.value} defaultChecked={props.defaultChecked} />
       </Container>
     </Wrapper>
   )
--- app/src/components/atoms/Form/Switch/style.tsx
+++ app/src/components/atoms/Form/Switch/style.tsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components'
 
+interface SwitchProps {
+  switch: boolean
+}
+
 export const Wrapper = styled.div`
   width: 121.992px;
+
+  &:focus {
+    border-color: #66afe9;
+    outline: 0;
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 2px rgba(102, 175, 233, .6);
+  }
 `
 
-export const Container = styled.div`
+export const Container = styled.div<SwitchProps>`
   width: 180px;
-  margin-left: 0px;
+  margin-left: ${props => props.switch ? 0 : -60}px;
 `
 
 export const Handle = styled.div`
