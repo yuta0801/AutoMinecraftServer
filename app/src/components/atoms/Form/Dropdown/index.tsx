3157d829db68ebe219667ad4844ae5f6fe7ed042
yuta0801 <yuta20030801@icloud.com>
2019-09-16 03:20:17 +0900

    Generalize Dropdown component

--- app/src/components/atoms/Form/Dropdown/index.tsx
+++ app/src/components/atoms/Form/Dropdown/index.tsx
@@ -2,19 +2,28 @@ import React, { useState, useRef } from 'react'
 import useOutsideClick from '../../../../hooks/useOutsideClick'
 import { Wrap, Button, Caret, Menu, Header, Item } from './style'
 
-type Option = {
+type Option<T> = {
   label?: string
-  value: string
+  value: T
 }
 
+export interface DropdownProps<T> {
   style?: React.CSSProperties
   containerStyle?: React.CSSProperties
-  options: Array<string | Option>
-  value: string
-  onChange(value: string): void
+  options: Array<string | Option<T>>
+  value: T
+  onChange(value: T): void
 }
 
-export const Dropdown = (props: DropdownProps) => {
+const getLabel = (options: Array<string | Option<any>>, value: any) => {
+  if (!value) return '選択'
+  const option = options.find(option =>
+    typeof option === 'object' && option.value === value
+  ) as Option<any>
+  return option.label || option.value || value
+}
+
+export const Dropdown = (props: DropdownProps<any>) => {
   const ref = useRef<HTMLAnchorElement>(null)
   useOutsideClick(ref, () => toggle(false))
 
@@ -23,7 +32,7 @@ export const Dropdown = (props: DropdownProps) => {
   return (
     <Wrap style={props.containerStyle}>
       <Button open={open} onClick={() => toggle(!open)} ref={ref} >
-        { props.value || '選択' }
+        { getLabel(props.options, props.value) }
         <Caret />
       </Button>
       { open && (
